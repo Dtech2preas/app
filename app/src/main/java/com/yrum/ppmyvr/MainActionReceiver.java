ad44392bf0de9c0bbf0a9603cfe02c069910648c
Dtech2preas <lefa4082@gmail.com>
2025-10-25 13:28:33 +0200

    Create MainActionReceiver.java

---
+++ app/src/main/java/com/yrum/ppmyvr/MainActionReceiver.java
@@ -0,0 +1,25 @@
+package com.yrum.ppmyvr;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MediaActionReceiver extends BroadcastReceiver {
+    private static final String TAG = "MediaActionReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d(TAG, "Media action received: " + action);
+
+        // Forward the action to MainActivity
+        Intent serviceIntent = new Intent(context, MusicService.class);
+        context.startService(serviceIntent);
+
+        Intent activityIntent = new Intent(context, MainActivity.class);
+        activityIntent.setAction(action);
+        activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(activityIntent);
+    }
+}
