c377f6530a699ea220f68076433ce584cac73bd2
Dtech2preas <lefa4082@gmail.com>
2025-10-25 13:27:46 +0200

    Create MusicService.java

---
+++ app/src/main/java/com/yrum/ppmyvr/MusicService.java
@@ -0,0 +1,50 @@
+package com.yrum.ppmyvr;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MusicService extends Service {
+    private static final String TAG = "MusicService";
+    private final IBinder binder = new LocalBinder();
+    
+    private boolean isPlaying = false;
+    private String currentSongName = "";
+    private String currentSongUri = "";
+
+    public class LocalBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Return STICKY to keep service running even if app is killed
+        return START_STICKY;
+    }
+
+    public void updatePlaybackState(boolean playing, String songName, String songUri) {
+        this.isPlaying = playing;
+        this.currentSongName = songName;
+        this.currentSongUri = songUri;
+        Log.d(TAG, "Playback state updated - Playing: " + playing + ", Song: " + songName);
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public String getCurrentSongName() {
+        return currentSongName;
+    }
+}
