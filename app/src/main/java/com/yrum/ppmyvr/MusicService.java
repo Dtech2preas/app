a008ad2693e139711e52d27bd5dd9da7672a0bc7
Dtech2preas <lefa4082@gmail.com>
2025-10-25 13:42:07 +0200

    Update MusicService.java

--- app/src/main/java/com/yrum/ppmyvr/MusicService.java
+++ app/src/main/java/com/yrum/ppmyvr/MusicService.java
@@ -2,8 +2,6 @@ package com.yrum.ppmyvr;
 
 import android.app.Service;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -46,5 +44,11 @@ public class MusicService extends Service {
 
     public String getCurrentSongName() {
         return currentSongName;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MusicService destroyed");
     }
 }
