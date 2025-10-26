1a44eece414cac2ce6da7be970eb10167ab85af5
Dtech2preas <lefa4082@gmail.com>
2025-10-26 23:20:31 +0200

    Update MusicService.java

--- app/src/main/java/com/yrum/ppmyvr/MusicService.java
+++ app/src/main/java/com/yrum/ppmyvr/MusicService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.v4.media.MediaMetadataCompat;
@@ -20,8 +19,8 @@ public class MusicService extends Service {
     private String currentSongName = "";
     private String currentSongUri = "";
 
+    // Media session for notifications
     private MediaSessionCompat mediaSession;
-    private MediaPlayer player;
 
     public class LocalBinder extends Binder {
         MusicService getService() {
@@ -33,15 +32,6 @@ public class MusicService extends Service {
     public void onCreate() {
         super.onCreate();
         initializeMediaSession();
-        initializePlayer();
-    }
-
-    private void initializePlayer() {
-        player = new MediaPlayer();
-        player.setOnCompletionListener(mp -> {
-            Log.d(TAG, "Song completed");
-            updatePlaybackState(false);
-        });
     }
 
     @Override
@@ -58,110 +48,6 @@ public class MusicService extends Service {
         mediaSession = new MediaSessionCompat(this, "D-TECH MUSIC");
         mediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
                               MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
-        mediaSession.setActive(true);
-
-        // Setup callback for controls from notification / lockscreen / Bluetooth
-        mediaSession.setCallback(new MediaSessionCompat.Callback() {
-            @Override
-            public void onPlay() {
-                super.onPlay();
-                Log.d(TAG, "onPlay called from system controls");
-                startPlayback();
-            }
-
-            @Override
-            public void onPause() {
-                super.onPause();
-                Log.d(TAG, "onPause called from system controls");
-                pausePlayback();
-            }
-
-            @Override
-            public void onStop() {
-                super.onStop();
-                Log.d(TAG, "onStop called from system controls");
-                stopPlayback();
-            }
-
-            @Override
-            public void onSkipToNext() {
-                super.onSkipToNext();
-                Log.d(TAG, "Next pressed");
-                sendBroadcast(new Intent("NEXT"));
-            }
-
-            @Override
-            public void onSkipToPrevious() {
-                super.onSkipToPrevious();
-                Log.d(TAG, "Previous pressed");
-                sendBroadcast(new Intent("PREVIOUS"));
-            }
-        });
-
-        updatePlaybackState(false);
-    }
-
-    // 🔊 Start playback
-    public void startPlayback() {
-        try {
-            if (currentSongUri == null || currentSongUri.isEmpty()) {
-                Log.w(TAG, "No song set to play.");
-                return;
-            }
-
-            if (player == null) initializePlayer();
-
-            if (player.isPlaying()) player.stop();
-            player.reset();
-
-            player.setDataSource(currentSongUri);
-            player.prepare();
-            player.start();
-
-            isPlaying = true;
-            updateMetadata();
-            updatePlaybackState(true);
-
-            Log.d(TAG, "Playback started: " + currentSongName);
-        } catch (Exception e) {
-            Log.e(TAG, "Error starting playback", e);
-        }
-    }
-
-    // ⏸ Pause playback
-    public void pausePlayback() {
-        if (player != null && player.isPlaying()) {
-            player.pause();
-            isPlaying = false;
-            updatePlaybackState(false);
-        }
-    }
-
-    // 🛑 Stop playback completely
-    public void stopPlayback() {
-        if (player != null) {
-            player.stop();
-            player.reset();
-            isPlaying = false;
-            updatePlaybackState(false);
-        }
-    }
-
-    private void updateMetadata() {
-        MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
-        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_TITLE,
-                                  currentSongName != null ? currentSongName : "D-TECH Music");
-        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ARTIST, "D-TECH MUSIC");
-        metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ALBUM, "D-TECH MUSIC");
-
-        Bitmap logo = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, logo);
-
-        mediaSession.setMetadata(metadataBuilder.build());
-    }
-
-    private void updatePlaybackState(boolean playing) {
-        int state = playing ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_PAUSED;
 
         PlaybackStateCompat.Builder stateBuilder = new PlaybackStateCompat.Builder()
                 .setActions(PlaybackStateCompat.ACTION_PLAY |
@@ -169,17 +55,87 @@ public class MusicService extends Service {
                             PlaybackStateCompat.ACTION_PLAY_PAUSE |
                             PlaybackStateCompat.ACTION_SKIP_TO_NEXT |
                             PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
-                            PlaybackStateCompat.ACTION_STOP)
-                .setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 1.0f);
+                            PlaybackStateCompat.ACTION_STOP);
 
         mediaSession.setPlaybackState(stateBuilder.build());
+        mediaSession.setActive(true);
+
+        mediaSession.setCallback(new MediaSessionCompat.Callback() {
+            @Override
+            public void onPlay() {
+                super.onPlay();
+                Log.d(TAG, "MediaSession: Play");
+                sendBroadcast(new Intent("PLAY"));
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                Log.d(TAG, "MediaSession: Pause");
+                sendBroadcast(new Intent("PAUSE"));
+            }
+
+            @Override
+            public void onSkipToNext() {
+                super.onSkipToNext();
+                Log.d(TAG, "MediaSession: Next");
+                sendBroadcast(new Intent("NEXT"));
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                super.onSkipToPrevious();
+                Log.d(TAG, "MediaSession: Previous");
+                sendBroadcast(new Intent("PREVIOUS"));
+            }
+
+            @Override
+            public void onStop() {
+                super.onStop();
+                Log.d(TAG, "MediaSession: Stop");
+                sendBroadcast(new Intent("STOP"));
+            }
+        });
     }
 
     public void updatePlaybackState(boolean playing, String songName, String songUri) {
+        this.isPlaying = playing;
         this.currentSongName = songName;
         this.currentSongUri = songUri;
-        if (playing) startPlayback();
-        else pausePlayback();
+
+        if (mediaSession != null) {
+            int state = playing ? PlaybackStateCompat.STATE_PLAYING : PlaybackStateCompat.STATE_PAUSED;
+
+            PlaybackStateCompat.Builder stateBuilder = new PlaybackStateCompat.Builder()
+                    .setActions(PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_NEXT |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_STOP)
+                    .setState(state, PlaybackStateCompat.PLAYBACK_POSITION_UNKNOWN, 1.0f);
+
+            mediaSession.setPlaybackState(stateBuilder.build());
+
+            // Metadata with D-TECH logo as default album art
+            MediaMetadataCompat.Builder metadataBuilder = new MediaMetadataCompat.Builder();
+            metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_TITLE,
+                                      (songName != null && !songName.isEmpty()) ? songName : "D-TECH Music");
+            metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ARTIST, "D-TECH MUSIC");
+            metadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ALBUM, "D-TECH MUSIC");
+
+            // Set D-TECH logo
+            Bitmap dtechLogo = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            metadataBuilder.putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, dtechLogo);
+
+            mediaSession.setMetadata(metadataBuilder.build());
+        }
+
+        Log.d(TAG, "Playback state updated - Playing: " + playing + ", Song: " + songName);
+    }
+
+    public MediaSessionCompat getMediaSession() {
+        return mediaSession;
     }
 
     public boolean isPlaying() {
@@ -193,11 +149,8 @@ public class MusicService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (player != null) {
-            player.release();
-            player = null;
-        }
         if (mediaSession != null) {
+            mediaSession.setActive(false);
             mediaSession.release();
         }
         Log.d(TAG, "MusicService destroyed");
